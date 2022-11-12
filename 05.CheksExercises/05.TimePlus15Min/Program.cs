547d967e059b1259eb50f51309da3f2183752339
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 12:06:39 +0200

    Add files via upload

---
+++ 05.CheksExercises/05.TimePlus15Min/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TimePlus15MIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hour = double.Parse(Console.ReadLine());
+            double min = double.Parse(Console.ReadLine());
+
+            min += 15;
+
+            if (min >= 60)
+            {
+                min = min - 60;
+                hour = hour + 1;
+            }
+
+            if (hour >= 24)
+            {
+                hour = 0;
+            }
+
+            string output = "";
+
+            if (min < 10)
+            {
+                output = $"{hour}:0{min}";
+            }
+            else
+            {
+                output = $"{hour}:{min}";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
