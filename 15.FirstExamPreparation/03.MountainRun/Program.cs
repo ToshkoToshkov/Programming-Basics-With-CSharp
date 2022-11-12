1977427b89c8ee843a7dbd5698fc1b5aa90d8d9d
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:13:04 +0200

    Add files via upload

---
+++ 15.FirstExamPreparation/03.MountainRun/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MountainRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordinSec = double.Parse(Console.ReadLine());
+            double meters = double.Parse(Console.ReadLine());
+            double secPerMeter = double.Parse(Console.ReadLine());
+
+            double totalTime = meters * secPerMeter;
+
+            double slow = Math.Floor(meters / 50) * 30;
+
+            double total = totalTime + slow;
+
+            if (total < recordinSec)
+            {
+                Console.WriteLine($"Yes! The new record is {total:F2} seconds.");
+            }
+            else
+            {
+                double difference = total - recordinSec;
+                Console.WriteLine($"No! He was {difference:F2} seconds slower.");
+            }
+        }
+    }
+}
