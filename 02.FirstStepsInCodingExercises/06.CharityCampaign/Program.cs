bec2f42c9d25bf37c2b3fb0f75383e97193f720d
ToshkoToshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 11:42:32 +0200

    Add files via upload

---
+++ 02.FirstStepsInCodingExercises/06.CharityCampaign/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CharityCampaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfTheDays = int.Parse(Console.ReadLine());
+            int numOfconfectioners = int.Parse(Console.ReadLine());
+
+            int numOfCakes = int.Parse(Console.ReadLine());
+            int numOfwaffles = int.Parse(Console.ReadLine());
+            int numOfCreps = int.Parse(Console.ReadLine());
+
+            double priceOfCake = 45;
+            double priceOfWaffle = 5.80;
+            double priceOfCrep = 3.20;
+
+            double cakesPrice = numOfCakes * priceOfCake;
+            double wafflesPrice = numOfwaffles * priceOfWaffle;
+            double crepsPrice = numOfCreps * priceOfCrep;
+
+            double moneyPerDay = (cakesPrice + wafflesPrice + crepsPrice) * numOfconfectioners;
+            double recruitedMoneyOfCampaign = moneyPerDay * numOfTheDays;
+
+            double expences = recruitedMoneyOfCampaign / 8;
+            double moneyAfterExpences = recruitedMoneyOfCampaign - expences;
+
+            Console.WriteLine($"{moneyAfterExpences:F2}");
+        }
+    }
+}
