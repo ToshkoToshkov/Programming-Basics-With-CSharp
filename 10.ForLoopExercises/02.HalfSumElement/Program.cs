2d3c50b5603bb9eb7acae0022dfd3a595c84be40
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 16:47:59 +0200

    Add files via upload

---
+++ 10.ForLoopExercises/02.HalfSumElement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int maxNum = int.MinValue;
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+
+                if (number > maxNum)
+                {
+                    maxNum = number;
+                }
+
+            }
+            sum -= maxNum;
+
+            if (sum == maxNum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(sum - maxNum)}");
+            }
+        }
+    }
+}
