be84f0791e523e50081754b9137b29a7cd5b05a6
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:00:31 +0200

    Add files via upload

---
+++ 12.WhileLoopExercise/01.OldBooks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchBook = Console.ReadLine();
+            string currentBook = Console.ReadLine();
+            int countBook = 0;
+            while (currentBook != "No More Books")
+            {
+                if (currentBook == searchBook)
+                {
+                    break;
+                }
+                countBook++;
+                currentBook = Console.ReadLine();
+            }
+
+            if (currentBook == searchBook)
+            {
+                Console.WriteLine($"You checked {countBook} books and found it.");
+            }
+            else if (currentBook == "No More Books")
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {countBook} books.");
+            }
+        }
+    }
+}
