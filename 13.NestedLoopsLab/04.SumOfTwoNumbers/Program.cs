13c9d91a69ffd1c6442548fda9a47e2c0685e83d
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:05:45 +0200

    Add files via upload

---
+++ 13.NestedLoopsLab/04.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int stop = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+
+            int numCombination = 0;
+            bool isTrue = false;
+
+            for (int i = start; i <= stop; i++)
+            {
+                for (int j = start; j <= stop; j++)
+                {
+                    numCombination++;
+
+                    if (i + j == magicNum)
+                    {
+                        isTrue = true;
+                        Console.WriteLine($"Combination N:{numCombination} ({i} + {j} = {magicNum})");
+                        break;
+                    }
+                }
+
+                if (isTrue)
+                {
+                    break;
+                }
+            }
+
+            if (!isTrue)
+            {
+                Console.WriteLine($"{numCombination} combinations - neither equals {magicNum}");
+            }
+        }
+    }
+}
