8eea614fb9b859f428d6bfceefeb6488d1f43cce
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 12:07:58 +0200

    Add files via upload

---
+++ 05.CheksExercises/08.Scholarship/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Scholarship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double income = double.Parse(Console.ReadLine());
+            double averageGrade = double.Parse(Console.ReadLine());
+            double minSalary = double.Parse(Console.ReadLine());
+
+            double socialScholarship = 0.0;
+            double succesScholarship = 0.0;
+            double studyScholarship = 0.0;
+
+            string output = "";
+
+            if (averageGrade >= 5.50)
+            {
+                succesScholarship = averageGrade * 25;
+                studyScholarship = succesScholarship;
+            }
+
+            if (averageGrade >= 4.50)
+            {
+                if (income < minSalary)
+                {
+                    socialScholarship = minSalary * 0.35;
+                    studyScholarship = socialScholarship;
+                }
+            }
+
+            if (socialScholarship > succesScholarship)
+            {
+                output = $"You get a Social scholarship {Math.Floor(socialScholarship)} BGN";
+            }
+
+            else if (socialScholarship <= succesScholarship)
+            {
+                output = $"You get a scholarship for excellent results {Math.Floor(succesScholarship)} BGN";
+            }
+
+            if (studyScholarship == 0)
+            {
+                output = "You cannot get a scholarship!";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
