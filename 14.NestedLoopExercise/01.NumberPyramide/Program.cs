b97e780a3d2092e3c657ae43dc45c6c5d90bbdc3
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:08:18 +0200

    Add files via upload

---
+++ 14.NestedLoopExercise/01.NumberPyramide/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NumberPyramide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            bool isBigger = false;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    count++;
+                    if (count > n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.Write(count + " ");
+                }
+
+                if (isBigger)
+                {
+                    break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
