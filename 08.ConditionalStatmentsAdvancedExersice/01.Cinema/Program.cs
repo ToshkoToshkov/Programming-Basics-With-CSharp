4e9f4dec7f5aad39fde8db8a892137b29d86a2b9
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 12:26:57 +0200

    Add files via upload

---
+++ 08.ConditionalStatmentsAdvancedExersice/01.Cinema/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int column = int.Parse(Console.ReadLine());
+
+            double income = 0.0;
+
+            if (type == "Premiere")
+            {
+                income = rows * column * 12;
+            }
+            else if (type == "Normal")
+            {
+                income = rows * column * 7.50;
+            }
+            else if (type == "Discount")
+            {
+                income = rows * column * 5;
+            }
+
+            Console.WriteLine($"{income:F2}" + " leva");
+        }
+    }
+}
