5af09043cdd6c3651a9a8b67c380841246f08bd8
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 12:04:56 +0200

    Add files via upload

---
+++ 05.CheksExercises/01.SumOfSeconds/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumOfSecods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstPlayer = int.Parse(Console.ReadLine());
+            int secondPlayer = int.Parse(Console.ReadLine());
+            int thirdPlayer = int.Parse(Console.ReadLine());
+
+            int totalTime = firstPlayer + secondPlayer + thirdPlayer;
+            double min = totalTime / 60;
+            double sec = totalTime % 60;
+
+            if (sec < 10)
+            {
+                Console.WriteLine($"{min}:0{sec}");
+            }
+            else
+            {
+                Console.WriteLine($"{min}:{sec}");
+            }
+        }
+    }
+}
