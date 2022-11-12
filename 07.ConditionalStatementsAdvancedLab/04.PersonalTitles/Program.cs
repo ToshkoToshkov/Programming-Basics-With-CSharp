c549f4be0a5523da2ebc3af198acdf4c8f22c2b0
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 12:13:08 +0200

    Add files via upload

---
+++ 07.ConditionalStatementsAdvancedLab/04.PersonalTitles/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PersonalTitles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age and gender and name");
+            double age = double.Parse(Console.ReadLine());
+            char gender = char.Parse(Console.ReadLine());
+            string name = Console.ReadLine();
+
+            if (gender == 'f')
+            {
+                if (age < 16)
+                {
+                    Console.WriteLine($"Miss {name}");
+                }
+                else if (age >= 16)
+                {
+                    Console.WriteLine($"Ms. {name}");
+                }
+            }
+            else if (gender == 'm')
+            {
+                if (age < 16)
+                {
+                    Console.WriteLine("Master");
+                }
+                else if (age >= 16)
+                {
+                    Console.WriteLine("Mr.");
+                }
+            }
+        }
+    }
+}
