32756bac9995813f073959e4b7a826f9189015e0
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 12:06:13 +0200

    Add files via upload

---
+++ 05.CheksExercises/04.MetricConverter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MetricConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            string result = Console.ReadLine();
+
+            if (input == "cm")
+            {
+                num /= 100;
+            }
+            if (input == "mm")
+            {
+                num /= 1000;
+            }
+
+            if (result == "cm")
+            {
+                num = num * 100;
+            }
+            else if (result == "mm")
+            {
+                num = num * 1000;
+            }
+
+            Console.WriteLine($"{num:F3}");
+        }
+    }
+}
