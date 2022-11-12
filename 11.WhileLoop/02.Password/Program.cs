4cc801606e41b9aa69f212b57be084325297203d
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 16:55:59 +0200

    Add files via upload

---
+++ 11.WhileLoop/02.Password/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == password)
+                {
+                    Console.WriteLine($"Welcome {username}!");
+                    break;
+                }
+            }
+        }
+    }
+}
