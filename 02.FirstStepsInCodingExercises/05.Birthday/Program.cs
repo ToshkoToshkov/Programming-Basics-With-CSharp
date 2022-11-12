f8326ab44c1ccac1139ead63132d64f3414febea
ToshkoToshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 11:42:04 +0200

    Add files via upload

---
+++ 02.FirstStepsInCodingExercises/05.Birthday/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Birthday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hallPrice = double.Parse(Console.ReadLine());
+
+            double cakePrice = hallPrice * 0.2;
+
+            double bevereges = cakePrice - cakePrice * 0.45;
+
+            double animatorPrice = hallPrice / 3;
+
+            double totalPrice = hallPrice + cakePrice + bevereges + animatorPrice;
+
+            Console.WriteLine(totalPrice);
+
+        }
+    }
+}
