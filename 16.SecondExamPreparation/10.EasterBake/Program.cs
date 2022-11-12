51d7f63cf87d39eafe06bf48d8d0b880af939e2e
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:24:03 +0200

    Add files via upload

---
+++ 16.SecondExamPreparation/10.EasterBake/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace EasterBake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfEasterBreads = int.Parse(Console.ReadLine());
+
+            double totalSugar = 0;
+            double totalFlour = 0;
+
+            int maxSugar = int.MinValue;
+            int maxFlour = int.MinValue;
+
+            for (int i = 1; i <= numOfEasterBreads; i++)
+            {
+                int sugar = int.Parse(Console.ReadLine());
+                int flour = int.Parse(Console.ReadLine());
+
+                totalSugar += sugar;
+                totalFlour += flour;
+
+                if (sugar > maxSugar)
+                {
+                    maxSugar = sugar;
+                }
+
+                if (flour > maxFlour)
+                {
+                    maxFlour = flour;
+                }
+            }
+
+            double numPacketsOfSugar = totalSugar / 950;
+            double numPacketsOfFlour = totalFlour / 750;
+
+            Console.WriteLine($"Sugar: {Math.Ceiling(numPacketsOfSugar)}");
+            Console.WriteLine($"Flour: {Math.Ceiling(numPacketsOfFlour)}");
+
+            Console.WriteLine($"Max used flour is {maxFlour} grams, max used sugar is {maxSugar} grams.");
+        }
+    }
+}
