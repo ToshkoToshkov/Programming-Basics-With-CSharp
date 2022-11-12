dd7db7335aca01c7708010639a5b24f1d13110ec
ToshkoToshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 11:40:59 +0200

    Add files via upload

---
+++ 02.FirstStepsInCodingExercises/03.DepositCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double initialSum = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double interestForMonth = initialSum * (percent * 0.01) / 12;
+            double result = initialSum + months * interestForMonth;
+
+            Console.WriteLine(result);
+        }
+    }
+}
