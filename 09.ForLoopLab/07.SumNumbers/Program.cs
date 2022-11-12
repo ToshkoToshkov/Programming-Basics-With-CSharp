4095f3c18c663f8fe6b340c9c3540a4a3be8cbfb
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 16:43:23 +0200

    Add files via upload

---
+++ 09.ForLoopLab/07.SumNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
