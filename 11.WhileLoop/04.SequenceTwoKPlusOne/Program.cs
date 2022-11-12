132cfd75528adb6b7f8c1e7db013a77a04755e2f
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 16:57:06 +0200

    Add files via upload

---
+++ 11.WhileLoop/04.SequenceTwoKPlusOne/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SequenceTwoKPlusOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int k = 1;
+
+            while (k <= n)
+            {
+                Console.WriteLine(k);
+                k = k * 2 + 1;
+            }
+        }
+    }
+}
