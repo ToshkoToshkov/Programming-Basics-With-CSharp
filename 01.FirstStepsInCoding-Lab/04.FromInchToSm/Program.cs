81444e6673b58a780b88281fb8f9fdecc9af07db
ToshkoToshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 11:30:32 +0200

    Add files via upload

---
+++ 01.FirstStepsInCoding-Lab/04.FromInchToSm/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FromInchToSm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inch = double.Parse(Console.ReadLine());
+
+            double sm = inch * 2.54;
+
+            Console.WriteLine(sm);
+        }
+    }
+}
