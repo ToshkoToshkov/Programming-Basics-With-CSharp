aa30981bda74875ebf50f3e05a90b33222710196
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:19:08 +0200

    Add files via upload

---
+++ 16.SecondExamPreparation/01.EasterLunch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EasterLunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfEasterBreads = int.Parse(Console.ReadLine());
+            int numberOfEggshels = int.Parse(Console.ReadLine());
+            int numberOfCookies = int.Parse(Console.ReadLine());
+
+            double priceOfEasterBread = numOfEasterBreads * 3.20;
+            double priceEggs = numberOfEggshels * 4.35;
+            double priceOfCookies = numberOfCookies * 5.40;
+            double numOfEggs = numberOfEggshels * 12;
+            double pricePaint = numOfEggs * 0.15;
+
+            double totalPrice = priceOfEasterBread + priceEggs + priceOfCookies + pricePaint;
+
+            Console.WriteLine($"{totalPrice:F2}");
+        }
+    }
+}
