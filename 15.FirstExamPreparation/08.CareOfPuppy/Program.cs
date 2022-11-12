50e36d7fc6a7409ced774f6d1feb89d97e4ccb98
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:15:39 +0200

    Add files via upload

---
+++ 15.FirstExamPreparation/08.CareOfPuppy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CareOfPuppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountOfFood = int.Parse(Console.ReadLine());
+            string gramsEatenAtEachMeal = Console.ReadLine();
+
+            int amountOfFoodInGrams = amountOfFood * 1000;
+
+            double amountOfFoodUntilAdoption = 0.0;
+
+            while (gramsEatenAtEachMeal != "Adopted")
+            {
+                double gramsEatenAt1Meal = double.Parse(gramsEatenAtEachMeal);
+                amountOfFoodUntilAdoption += gramsEatenAt1Meal;
+
+
+                gramsEatenAtEachMeal = Console.ReadLine();
+            }
+
+            if (amountOfFoodInGrams >= amountOfFoodUntilAdoption)
+            {
+                double restedFood = amountOfFoodInGrams - amountOfFoodUntilAdoption;
+                Console.WriteLine($"Food is enough! Leftovers: {restedFood} grams.");
+            }
+            else
+            {
+                double neededFood = amountOfFoodUntilAdoption - amountOfFoodInGrams;
+                Console.WriteLine($"Food is not enough. You need {neededFood} grams more.");
+            }
+
+        }
+    }
+}
