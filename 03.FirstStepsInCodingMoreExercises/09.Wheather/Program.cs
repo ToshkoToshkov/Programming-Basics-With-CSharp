bb0fa1095d7218adc2cb6649729563b1aa2ff5eb
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 11:58:36 +0200

    Add files via upload

---
+++ 03.FirstStepsInCodingMoreExercises/09.Wheather/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Wheather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "sunny")
+            {
+                Console.WriteLine("It's warm outside!");
+            }
+            else
+            {
+                Console.WriteLine("It's cold outside!");
+            }
+
+        }
+    }
+}
