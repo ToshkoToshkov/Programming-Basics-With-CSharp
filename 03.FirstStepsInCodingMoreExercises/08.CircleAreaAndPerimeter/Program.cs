d143620cd149359f77933c8687f42ef86c2f826e
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 11:58:01 +0200

    Add files via upload

---
+++ 03.FirstStepsInCodingMoreExercises/08.CircleAreaAndPerimeter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CircleAreaAndPerimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+
+            double areaCircle = Math.PI * (r * r);
+
+            double perimeterCircle = 2 * Math.PI * r;
+
+            Console.WriteLine($"{areaCircle:F2}");
+            Console.WriteLine($"{perimeterCircle:F2}");
+        }
+    }
+}
