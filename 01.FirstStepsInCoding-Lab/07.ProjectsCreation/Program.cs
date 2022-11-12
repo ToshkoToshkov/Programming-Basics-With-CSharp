d0fcc970865e5c8974b6aed78ab37f267d2080fb
ToshkoToshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 11:32:05 +0200

    Add files via upload

---
+++ 01.FirstStepsInCoding-Lab/07.ProjectsCreation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProjectsCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameArchitect = Console.ReadLine();
+            int numOfProjects = int.Parse(Console.ReadLine());
+
+            double timeForAllProjects = numOfProjects * 3;
+
+            Console.WriteLine("The architect " + nameArchitect + " will need " + timeForAllProjects + " hours to complete " + numOfProjects + " project/s.");
+        }
+    }
+}
