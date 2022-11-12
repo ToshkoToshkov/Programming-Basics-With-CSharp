108c67f3658f6526bd4e49352b11487a1854090c
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:03:06 +0200

    Add files via upload

---
+++ 12.WhileLoopExercise/06.Cake/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+
+            int piecesCake = lenght * width;
+            string line = Console.ReadLine();
+            bool isCakeEnough = true;
+
+            while (line != "STOP")
+            {
+                int currentPieces = int.Parse(line);
+                piecesCake -= currentPieces;
+                if (piecesCake < 0)
+                {
+                    isCakeEnough = false;
+                    break;
+                }
+
+                line = Console.ReadLine();
+            }
+
+            if (isCakeEnough)
+            {
+                Console.WriteLine($"{piecesCake} pieces are left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(piecesCake)} pieces more.");
+            }
+        }
+    }
+}
