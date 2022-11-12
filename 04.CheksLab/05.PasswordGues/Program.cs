60e870ce5ae9a81e50977bc548c4f22546e21a41
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 12:02:19 +0200

    Add files via upload

---
+++ 04.CheksLab/05.PasswordGues/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PasswordGues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            string greatPassword = "s3cr3t!P@ssw0rd";
+
+            if (password == greatPassword)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
