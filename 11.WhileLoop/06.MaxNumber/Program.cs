473fb97a3034aaf1cb36f34471843fac9023175b
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 16:57:58 +0200

    Add files via upload

---
+++ 11.WhileLoop/06.MaxNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int nums = 0;
+            int maxNum = int.MinValue;
+
+            while (input != "Stop")
+            {
+                nums = int.Parse(input);
+                if (nums > maxNum)
+                {
+                    maxNum = nums;
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(maxNum);
+        }
+    }
+}
