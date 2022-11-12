f55ca6091e71dac33f796c9ee7b88036acda09bc
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:12:37 +0200

    Add files via upload

---
+++ 15.FirstExamPreparation/02.CatWalking/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CatWalking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minWalk = int.Parse(Console.ReadLine());
+            int numWalk = int.Parse(Console.ReadLine());
+            int caloriesPerDay = int.Parse(Console.ReadLine());
+
+            double totalMinPerDay = minWalk * numWalk;
+            double calories = totalMinPerDay * 5;
+            double percent = caloriesPerDay / 2;
+
+            if (calories >= percent)
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {calories}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {calories}.");
+            }
+
+
+        }
+    }
+}
