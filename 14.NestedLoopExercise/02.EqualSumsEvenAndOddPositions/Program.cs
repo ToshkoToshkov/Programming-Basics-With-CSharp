62c52c6c3f63c2ceb93a91763634e7e54e43c88c
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:08:44 +0200

    Add files via upload

---
+++ 14.NestedLoopExercise/02.EqualSumsEvenAndOddPositions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EqualSumsEvenAndOddPositions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            for (int i = n1; i <= n2; i++)
+            {
+                int oddSum = 0;
+                int evenSum = 0;
+
+                int number = i;
+
+                for (int position = 6; position >= 1; position--)
+                {
+                    int digit = number % 10;
+                    number = number / 10;
+
+                    if (position % 2 == 0)
+                    {
+                        evenSum += digit;
+                    }
+                    else
+                    {
+                        oddSum += digit;
+                    }
+                }
+
+                if (oddSum == evenSum)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
