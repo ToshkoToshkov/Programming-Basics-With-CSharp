f0e01439ecfe75fb5293b95f1c84e826be9d9cc3
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:00:58 +0200

    Add files via upload

---
+++ 12.WhileLoopExercise/02.ExamPreparation/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int poorGradesLimit = int.Parse(Console.ReadLine());
+            string task = Console.ReadLine();
+
+            bool isExcelent = true;
+            int poorGrades = 0;
+            double score = 0;
+            int allGrades = 0;
+            string lastProblem = "";
+
+            while (task != "Enough")
+            {
+                int grade = int.Parse(Console.ReadLine());
+
+                score += grade;
+                allGrades++;
+                lastProblem = task;
+
+                if (grade <= 4)
+                {
+                    poorGrades++;
+                    if (poorGrades == poorGradesLimit)
+                    {
+                        isExcelent = false;
+                        break;
+                    }
+                }
+
+                task = Console.ReadLine();
+            }
+
+            double averageGrade = score / allGrades;
+
+            if (isExcelent)
+            {
+                Console.WriteLine($"Average score: {averageGrade:F2}");
+                Console.WriteLine($"Number of problems: {allGrades}");
+                Console.WriteLine($"Last problem: {lastProblem}");
+            }
+            else
+            {
+                Console.WriteLine($"You need a break, {poorGrades} poor grades.");
+            }
+        }
+    }
+}
