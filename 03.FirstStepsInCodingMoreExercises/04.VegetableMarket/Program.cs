f60b30f08e80b00d822af766c1a1097080d7a40e
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 11:55:43 +0200

    Add files via upload

---
+++ 03.FirstStepsInCodingMoreExercises/04.VegetableMarket/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VegetableMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pricePerKilVeget = double.Parse(Console.ReadLine());
+            double pricePerKilFruits = double.Parse(Console.ReadLine());
+            int kgVeget = int.Parse(Console.ReadLine());
+            int kgFruits = int.Parse(Console.ReadLine());
+
+            double totalPriceVeget = pricePerKilVeget * kgVeget;
+            double totalPriceFruits = pricePerKilFruits * kgFruits;
+
+            double totalPriceInLv = totalPriceFruits + totalPriceVeget;
+
+            double totalPriceInEuro = totalPriceInLv / 1.94;
+
+            Console.WriteLine($"{totalPriceInEuro:F2}");
+        }
+    }
+}
