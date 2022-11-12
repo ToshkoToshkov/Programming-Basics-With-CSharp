4f945e4c1f33eb13d210623a37c1a98ea82c43f1
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 12:07:32 +0200

    Add files via upload

---
+++ 05.CheksExercises/07.WorldSwimingRecord/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WorldSwimingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recInSec = double.Parse(Console.ReadLine());
+            double distanceInMetters = double.Parse(Console.ReadLine());
+            double timeInSecPerOneMetter = double.Parse(Console.ReadLine());
+
+            double distance = distanceInMetters * timeInSecPerOneMetter;
+            double delay = Math.Floor(distanceInMetters / 15) * 12.5;
+            double totalTime = distance + delay;
+
+            if (totalTime < recInSec)
+            {
+                Console.WriteLine("Yes, he succeeded! The new world record is " + $"{totalTime:F2}" + " seconds.");
+
+            }
+            else
+            {
+                double secSlower = totalTime - recInSec;
+                Console.WriteLine("No, he failed! He was " + $"{secSlower:F2}" + " seconds slower.");
+            }
+        }
+    }
+}
