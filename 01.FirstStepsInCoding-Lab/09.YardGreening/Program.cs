c36762906d09aadfe218e08eb40b12beaad9e042
ToshkoToshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 11:33:22 +0200

    Add files via upload

---
+++ 01.FirstStepsInCoding-Lab/09.YardGreening/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mettersForGreening = double.Parse(Console.ReadLine());
+
+            double totalPriceForGreening = mettersForGreening * 7.61;
+            double theDiscount = 0.18 * totalPriceForGreening;
+            double finalPrice = totalPriceForGreening - theDiscount;
+
+            Console.WriteLine("The final price is: " + finalPrice + " lv.");
+            Console.WriteLine("The discount is: " + theDiscount + " lv.");
+
+        }
+    }
+}
