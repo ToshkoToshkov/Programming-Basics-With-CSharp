3a00858c31792d918306259cb4d0ca96009fc4ce
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 11:54:52 +0200

    Add files via upload

---
+++ 03.FirstStepsInCodingMoreExercises/02.TriangleArea/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double side = double.Parse(Console.ReadLine());
+            double hight = double.Parse(Console.ReadLine());
+
+            double area = side * hight / 2;
+
+            Console.WriteLine($"{area:F2}");
+        }
+    }
+}
