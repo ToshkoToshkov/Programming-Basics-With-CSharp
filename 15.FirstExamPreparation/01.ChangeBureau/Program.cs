b854333963938e2e81cc28c030ca6f3ccf40b924
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:12:04 +0200

    Add files via upload

---
+++ 15.FirstExamPreparation/01.ChangeBureau/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ChangeBureau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numBitcoins = int.Parse(Console.ReadLine());
+            double numYuans = double.Parse(Console.ReadLine());
+            double commision = double.Parse(Console.ReadLine());
+
+            double bitcoinInLv = numBitcoins * 1168;
+            double yoanInDOlars = numYuans * 0.15;
+            double dolarsInLv = yoanInDOlars * 1.76;
+            double totalLv = bitcoinInLv + dolarsInLv;
+            double totalEuro = totalLv / 1.95;
+            double totalMoney = totalEuro - (totalEuro * commision / 100);
+            Console.WriteLine($"{totalMoney:F2}");
+        }
+    }
+}
