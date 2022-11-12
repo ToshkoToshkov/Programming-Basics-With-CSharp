db35afa4eea71f298b77e2fb6a3934e9496ca64b
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 11:57:00 +0200

    Add files via upload

---
+++ 03.FirstStepsInCodingMoreExercises/06.FishLand/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fishland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceMackerel = double.Parse(Console.ReadLine());
+            double priceSprinkle = double.Parse(Console.ReadLine());
+            double kgBonito = double.Parse(Console.ReadLine());
+            double kgSafrid = double.Parse(Console.ReadLine());
+            int kgClams = int.Parse(Console.ReadLine());
+
+            double priceBonito = priceMackerel + priceMackerel * 0.60;
+            double priceSafrid = priceSprinkle + priceSprinkle * 0.80;
+
+            double totalPriceBonito = kgBonito * priceBonito;
+            double totalPriceSafrid = kgSafrid * priceSafrid;
+            double totalPriceClams = kgClams * 7.50;
+
+            double moneyToPay = totalPriceClams + totalPriceBonito + totalPriceSafrid;
+
+            Console.WriteLine($"{moneyToPay:F2}");
+
+        }
+    }
+}
