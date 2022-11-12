96bb44641b7b02447366459940473018aeea3de1
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 16:57:32 +0200

    Add files via upload

---
+++ 11.WhileLoop/05.AccountBalance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            double currentINcrease = 0.0;
+            double bankAccount = 0.0;
+
+            while (input != "NoMoreMoney")
+            {
+                currentINcrease = double.Parse(input);
+
+                if (currentINcrease < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+
+                bankAccount += currentINcrease;
+
+                Console.WriteLine($"Increase: {currentINcrease:F2}");
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total: {bankAccount:F2}");
+        }
+    }
+}
