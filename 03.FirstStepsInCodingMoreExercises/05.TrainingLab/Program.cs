c0e59fa6be1dba369706a1255d5ba6220615ddb7
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 11:56:28 +0200

    Add files via upload

---
+++ 03.FirstStepsInCodingMoreExercises/05.TrainingLab/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Transactions;
+
+namespace TrainingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double h = double.Parse(Console.ReadLine());
+            double w = double.Parse(Console.ReadLine());
+
+            double hCm = h * 100;
+            double wCm = w * 100 - 100;
+            double chekH = hCm / 120;
+            double chekH2 = Math.Floor(chekH);
+            double chekW = wCm / 70;
+            double chekW2 = Math.Floor(chekW);
+            double totalDesks = chekW2 * chekH2 - 3;
+
+            Console.WriteLine(totalDesks);
+        }
+    }
+}
