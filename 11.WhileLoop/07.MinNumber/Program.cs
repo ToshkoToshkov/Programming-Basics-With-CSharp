1c8cee547bc722ced7af2c2ce09f480dc3131d7e
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 16:58:27 +0200

    Add files via upload

---
+++ 11.WhileLoop/07.MinNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int nums = 0;
+            int minNum = int.MaxValue;
+
+            while (input != "Stop")
+            {
+                nums = int.Parse(input);
+
+                if (nums < minNum)
+                {
+                    minNum = nums;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(minNum);
+        }
+    }
+}
