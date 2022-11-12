7e53dbbc0033f18484e5711d9111772a58f76fa1
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:06:13 +0200

    Add files via upload

---
+++ 13.NestedLoopsLab/05.Traveling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Traveling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+
+            while (destination != "End")
+            {
+                double cost = double.Parse(Console.ReadLine());
+                double earnedSaving = 0;
+
+                while (cost > earnedSaving)
+                {
+                    earnedSaving += double.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
