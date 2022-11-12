4e68c9dadd09e70bc9752ec4d3e49eca74612199
ToshkoToshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 11:43:06 +0200

    Add files via upload

---
+++ 02.FirstStepsInCodingExercises/07.Acuarium/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Acuarium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenghtInCm = int.Parse(Console.ReadLine());
+            int widthInCm = int.Parse(Console.ReadLine());
+            int heightInCm = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double volumeAcuarium = lenghtInCm * widthInCm * heightInCm;
+
+            double lettersInAcuarium = volumeAcuarium * 0.001;
+
+            double percentIn = percent * 0.01;
+
+            double lettersWeNeed = lettersInAcuarium * (1 - percentIn);
+
+            Console.WriteLine(lettersWeNeed);
+        }
+    }
+}
