d0ea4da9781fcaaa27220167c9fa63cd041edc3c
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 16:54:06 +0200

    Add files via upload

---
+++ 10.ForLoopExercises/06.Salary/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int openTabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            string nameWebsite = "";
+            int left = 0;
+
+
+            for (int i = 1; i <= openTabs; i++)
+            {
+                nameWebsite = Console.ReadLine();
+
+                if (nameWebsite == "Facebook")
+                {
+                    salary -= 150;
+                }
+                else if (nameWebsite == "Instagram")
+                {
+                    salary -= 100;
+                }
+                else if (nameWebsite == "Reddit")
+                {
+                    salary -= 50;
+                }
+            }
+
+            if (salary <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
