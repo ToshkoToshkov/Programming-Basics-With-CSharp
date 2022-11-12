1a09024daf69499c11b0881a91719bb3e45f4bc8
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 16:45:10 +0200

    Add files via upload

---
+++ 09.ForLoopLab/10.EvenOddSum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EvenOddSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int nums = 0;
+            int sumEven = 0;
+            int sumOdd = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                nums = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    sumEven += nums;
+                }
+                else
+                {
+                    sumOdd += nums;
+                }
+
+            }
+
+            if (sumEven == sumOdd)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + sumEven);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + Math.Abs(sumEven - sumOdd));
+            }
+        }
+    }
+}
