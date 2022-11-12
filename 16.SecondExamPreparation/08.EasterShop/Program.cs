0ce05fefc95ab7904065d6ad8002fbfaa29a0e78
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:23:02 +0200

    Add files via upload

---
+++ 16.SecondExamPreparation/08.EasterShop/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace EasterShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Eggs = int.Parse(Console.ReadLine());
+            string action = Console.ReadLine();
+            int numEggs = int.Parse(Console.ReadLine());
+
+            int sellEggs = 0;
+
+            while (action != "Close")
+            {
+                if (action == "Buy")
+                {
+                    if (numEggs > Eggs)
+                    {
+                        Console.WriteLine("Not enough eggs in store!");
+                        Console.WriteLine($"You can buy only {Eggs}.");
+                        break;
+                    }
+
+                    Eggs -= numEggs;
+                    sellEggs += numEggs;
+                }
+                else if (action == "Fill")
+                {
+                    Eggs += numEggs;
+                }
+
+
+                action = Console.ReadLine();
+
+                if (action == "Close")
+                {
+                    Console.WriteLine("Store is closed!");
+                    Console.WriteLine($"{sellEggs} eggs sold.");
+                    break;
+                }
+
+                numEggs = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
