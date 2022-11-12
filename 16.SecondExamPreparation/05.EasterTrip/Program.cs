af6264f784628387d1459470c7702eb6c81b4454
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:21:30 +0200

    Add files via upload

---
+++ 16.SecondExamPreparation/05.EasterTrip/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace EasterTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            string days = Console.ReadLine();
+            int numNights = int.Parse(Console.ReadLine());
+
+            double price = 0.0;
+
+            switch (destination)
+            {
+                case "France":
+                    if (days == "21-23")
+                    {
+                        price = numNights * 30;
+                    }
+                    else if (days == "24-27")
+                    {
+                        price = numNights * 35;
+                    }
+                    else if (days == "28-31")
+                    {
+                        price = numNights * 40;
+                    }
+                    break;
+
+                case "Italy":
+                    if (days == "21-23")
+                    {
+                        price = numNights * 28;
+                    }
+                    else if (days == "24-27")
+                    {
+                        price = numNights * 32;
+                    }
+                    else if (days == "28-31")
+                    {
+                        price = numNights * 39;
+                    }
+                    break;
+
+                case "Germany":
+                    if (days == "21-23")
+                    {
+                        price = numNights * 32;
+                    }
+                    else if (days == "24-27")
+                    {
+                        price = numNights * 37;
+                    }
+                    else if (days == "28-31")
+                    {
+                        price = numNights * 43;
+                    }
+                    break;
+            }
+
+            Console.WriteLine($"Easter trip to {destination} : {price:F2} leva.");
+        }
+    }
+}
