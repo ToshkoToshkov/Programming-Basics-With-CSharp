fd93a381ec7647cc2d22bbc58a19f918b3b41c84
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 12:14:28 +0200

    Add files via upload

---
+++ 07.ConditionalStatementsAdvancedLab/07.WorkingHours/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace WorkingHours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hour = double.Parse(Console.ReadLine());
+            string dayOfWeek = Console.ReadLine();
+
+            switch (dayOfWeek)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                case "Saturday":
+                    if (hour >= 10 && hour <= 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    else if (hour >= 0 && hour < 10)
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    else if (hour > 18 && hour <= 23)
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+                case "Sunday":
+                    Console.WriteLine("closed");
+                    break;
+            }
+        }
+    }
+}
