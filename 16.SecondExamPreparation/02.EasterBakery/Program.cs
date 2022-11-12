ff4783c4c461c3b8dd289cefce2eae81f5accb61
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 17:19:40 +0200

    Add files via upload

---
+++ 16.SecondExamPreparation/02.EasterBakery/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EasterBakery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceOfKgFlour = double.Parse(Console.ReadLine());
+            double kgFlour = double.Parse(Console.ReadLine());
+            double kgSugar = double.Parse(Console.ReadLine());
+            int numOFEggshels = int.Parse(Console.ReadLine());
+            int packetOfYeast = int.Parse(Console.ReadLine());
+
+            double priceFlour = priceOfKgFlour * kgFlour;
+
+            double priceKgSugar = priceOfKgFlour - (priceOfKgFlour * 0.25);
+            double priceSugar = kgSugar * priceKgSugar;
+
+            double priceEggshell = priceOfKgFlour + (priceOfKgFlour * 0.1);
+            double priceOFEggs = priceEggshell * numOFEggshels;
+
+            double pricePacketOfYeast = priceKgSugar - (priceKgSugar * 0.8);
+            double priceYeast = pricePacketOfYeast * packetOfYeast;
+
+            double totalPrice = priceFlour + priceSugar + priceOFEggs + priceYeast;
+
+            Console.WriteLine($"{totalPrice:F2}");
+
+        }
+    }
+}
