0880d58c2f23e48b527f638a0ed316faf508c429
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 12:07:05 +0200

    Add files via upload

---
+++ 05.CheksExercises/06.GodzilaVsKingkong/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GodzilaVsKingkong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int numOfExtras = int.Parse(Console.ReadLine());
+            double priceClothingForExtra = double.Parse(Console.ReadLine());
+
+            double priceDecor = budget * 0.1;
+
+
+            if (numOfExtras > 150)
+            {
+                priceClothingForExtra = priceClothingForExtra - (priceClothingForExtra * 0.1);
+            }
+            else
+            {
+                priceClothingForExtra = priceClothingForExtra;
+            }
+
+            double totalPriceExtras = numOfExtras * priceClothingForExtra;
+
+            double expencesForDecorAndClothing = priceDecor + totalPriceExtras;
+
+            if (expencesForDecorAndClothing > budget)
+            {
+                double neededMoney = expencesForDecorAndClothing - budget;
+
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine("Wingard needs " + $"{neededMoney:F2}" + " leva more.");
+
+            }
+            else
+            {
+                double moneyLeft = budget - expencesForDecorAndClothing;
+                Console.WriteLine("Action!");
+                Console.WriteLine("Wingard starts filming with " + $"{moneyLeft:F2}" + " leva left.");
+
+            }
+        }
+    }
+}
