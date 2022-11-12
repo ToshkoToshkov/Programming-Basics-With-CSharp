c962b89bfed22eba832e09209ed31f97b216b3f7
ToshkoToshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-12 11:32:47 +0200

    Add files via upload

---
+++ 01.FirstStepsInCoding-Lab/08.PetShop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfDogs = int.Parse(Console.ReadLine());
+            int numOfOtherAnimals = int.Parse(Console.ReadLine());
+
+            double priceFoodForDogs = numOfDogs * 2.50;
+            double priceFoodForOtherAnimals = numOfOtherAnimals * 4;
+
+            double totalPrice = priceFoodForDogs + priceFoodForOtherAnimals;
+
+            Console.WriteLine(totalPrice + " lv.");
+        }
+    }
+}
